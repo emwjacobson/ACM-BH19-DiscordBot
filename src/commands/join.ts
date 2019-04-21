9ced60bbca49bb9f82142421778d6406cdc665d1
Emerson Jacobson <emwjacobson@gmail.com>
2019-04-21 07:52:22 -0700

    Bot should now join voice channels correctly.

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -3,17 +3,19 @@ import { Message, Collection, GuildMember, DMChannel, VoiceConnection, User } fr
 import { env } from "../environment";
 
 export class Join extends Command {
+    // private voice_buffers: Map<>
+    
     constructor() {
         super('join');
     }
 
     public call(msg: Message, command: string) {
-        console.log(msg.member.voiceChannel.joinable);
         msg.member.voiceChannel.join().then((connection: VoiceConnection) => {
             msg.channel.send("Here I come!");
+            // connection.playFile("C:/crab.mp3");
             let reciever = connection.createReceiver();
             reciever.on('pcm', (user: User, buffer: Buffer) => {
-                console.log("STUFF IS HAPPENING!!!!");
+                console.log(user.username, "is talking", buffer);
             });
         }).catch((error) => {
             msg.channel.send("Hm, there seems to be a problem...");
