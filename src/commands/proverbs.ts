160083d901985a19ac8c532f619cc9cdcc2a9517
globalnet626 <miguelslvn@gmail.com>
2019-04-21 14:06:17 -0700

    comit

---
+++ src/commands/proverbs.ts
@@ -0,0 +1,15 @@
+export class Proverbs {
+    public proverbs : string[] = new Array(
+        "Drown in the salt sea", 
+    "Submit yourself to chaos", 
+    "Return to the darkness")
+
+    constructor() 
+    {
+    }
+
+    public read()
+    {
+        return this.proverbs[Math.round(Math.floor(this.proverbs.length - 1) * Math.random())]
+    }
+}
