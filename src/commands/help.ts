e1402600fc9e33a5aff94312101979cf3a01c0a6
Mary Kelly <maryalinakelly@gmail.com>
2019-04-21 20:38:04 +0000

    changed help message

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -8,9 +8,12 @@ export class Help extends Command {
     }
 
     public call(msg: Message, command: string) {
-        let help_msg = "@TiaBot to \n";
-        help_msg += "startbr - starts a BR session";
-        help_msg += "endbr - ends a BR session";
+        let help_msg = "Powers of TiaBot: \n";
+        help_msg += "kick [@username(s)] throws user(s) overboard \n";
+        help_msg += "ban [@username(s)] returns user(s) to the cosmic abyss \n";
+        help_msg += "tablet-of-destinies instigates a primordial battle between (non-admin) users. \n";
+        help_msg += "Users that fail to please the goddess are cast into the sea. Last user remaining has the \n";
+        help_msg += "Tablet of Destinies conferred on them by TiaBot, making them the new ruler of lesser users. \n";
         msg.channel.send(help_msg);
     }
 }
